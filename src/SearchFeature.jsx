fc325b869e391d8938a0b8adc23d17ed34a89941
mohammedasimahmed <123298280+mohammedasimahmed@users.noreply.github.com>
2023-06-17 17:37:19 +0530

    Update SearchFeature.jsx

--- src/SearchFeature.jsx
+++ src/SearchFeature.jsx
@@ -1,31 +1,31 @@
 import Suggestions from "./Suggestions";
 import VoiceSearch from "./VoiceSearch";
 
-export default function SearchFeature({ handleSubmit, state, setState  ,filterSugg , suggClick ,val ,setVal , rmsug }) {
+export default function SearchFeature({ handleSubmit, state, setState, filterSugg, suggClick, val, setVal, rmsug }) {
     return (
         <>
             <h1 className="title">Cars</h1>
             <label htmlFor="search" className="labelCarSearch">Vehicle Search , Search by Name or by Type</label>
             <form action="" onSubmit={handleSubmit} className="searchForm">
                 <div className="searchContainer">
-                    <input 
-                    type="text" 
-                    value={state} 
-                    onChange={(e) => setState(e.target.value)} 
-                    className="searchBar" 
-                    id="search"
-                    autoComplete="off"
+                    <input
+                        type="text"
+                        value={state}
+                        onChange={(e) => setState(e.target.value)}
+                        className="searchBar"
+                        id="search"
+                        autoComplete="off"
                     />
-                    <Suggestions 
-                    filterSugg={filterSugg} 
-                    suggClick={suggClick} 
-                    rmsug={rmsug}
+                    <Suggestions
+                        filterSugg={filterSugg}
+                        suggClick={suggClick}
+                        rmsug={rmsug}
                     />
-                                <VoiceSearch 
-                setState={setState} 
-                val={val}
-                setVal={setVal}
-            />
+                    <VoiceSearch
+                        setState={setState}
+                        val={val}
+                        setVal={setVal}
+                    />
                     <button className="searchBtn" >SEARCH</button>
                 </div>
                 {/* <div>hi</div> */}
